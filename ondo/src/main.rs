f7a60f44c54e25a96cb2ec1332bce40d73e5ad7f
Umur Ozkul <umur@hightechmind.io>
2022-11-18 17:59:51 +0100

    test pattern

--- ondo/src/main.rs
+++ ondo/src/main.rs
@@ -1,3 +1,17 @@
+pub fn hello() -> String {
+    "Hello, world!".to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello());
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello(), "Hello, world!");
+    }
 }
