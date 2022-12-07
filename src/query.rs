ce7ef0817fdc65ca728c29f38ec1835d4a18c9d5
Umur Ozkul <umur@hightechmind.io>
2022-12-07 20:49:01 +0100

    request

--- src/query.rs
+++ src/query.rs
@@ -2,11 +2,13 @@ use crate::{index_definition::IndexDefinition, types::*};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Query {
-    CreateTable(DomainName, TableName),
-    DeleteTabel(DomainName, TableName),
-    CreateIndex(DomainName, IndexDefinition),
+    PostTable(DomainName, TableName),
+    DeleteTable(DomainName, TableName),
+    PostIndex(DomainName, IndexDefinition),
     DeleteIndex(DomainName, TableName, IndexName),
     Get(DomainName, TableName, Key),
+    Find(DomainName, TableName, IndexName, Key),
+    Put(DomainName, TableName, Key, Value),
+    Post(DomainName, TableName, Key, Value),
     Delete(DomainName, TableName, Key),
-    Find(DomainName, TableName, IndexName, Key)
 }
