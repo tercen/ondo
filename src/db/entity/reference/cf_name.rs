472502555f9153494ab7b210cb5436216e440f15
Umur Ozkul <umur@hightechmind.io>
2022-12-19 17:14:13 +0100

    update cf_name

--- src/db/entity/reference/cf_name.rs
+++ src/db/entity/reference/cf_name.rs
@@ -7,26 +7,26 @@ pub struct CfName;
 impl CfName {
     pub fn for_server_meta() -> String {
         //table names etc
-        "/server/_".to_string()
+        "/server".to_string()
     }
 
     pub fn for_domain_meta() -> String {
         //table names etc
-        "/domains/_".to_string()
+        "/domains".to_string()
     }
 
     pub fn for_table_meta(r: &DomainReference) -> String {
         //indexes etc
-        format!("/domains/{}/tables/_", r.domain_name)
+        format!("/domains/{}/tables", r.domain_name)
     }
 
     pub fn for_table_values(r: &TableReference) -> String {
         //value rows
-        format!("{}::/{}/_", r.domain_name, r.table_name)
+        format!("{}::/{}", r.domain_name, r.table_name)
     }
 
     pub fn for_index_values(r: &IndexReference) -> String {
-        format!("{}::/{}/indexes/{}/_", r.domain_name, r.table_name, r.index_name)
+        format!("{}::/{}/indexes/{}", r.domain_name, r.table_name, r.index_name)
     }
 }
 
