aab19815ba38538d35a91fac1370795f92971dcd
Umur Ozkul <umur@hightechmind.io>
2023-03-14 12:13:59 +0100

    rocks db cf commands

--- src/db/server/source_sink/effects_sink.rs
+++ src/db/server/source_sink/effects_sink.rs
@@ -1,3 +1,4 @@
+use crate::db::db_error::DbError;
 use crate::db::server::db_error_to_status::DbErrorToStatus;
 use crate::db::{entity::reference::effect::Effect, server::rocks_db_accessor::RocksDbAccessor};
 use crate::ondo_remote::EmptyMessage;
@@ -9,34 +10,36 @@ pub(in crate::db::server) trait EffectsSink {
 
 impl EffectsSink for Vec<Effect> {
     fn apply_effects(&self, ra: &RocksDbAccessor) -> Result<Response<EmptyMessage>, Status> {
-        let _db = ra.db_guard().map_db_err_to_status()?;
-        self.into_iter().for_each(|effect| match effect {
-            Effect::CreateCf(_cf_name) => {
-                todo!();
-                // db.create_cf(cf_name).map_db_err_to_status();
-            }
-            Effect::DeleteCf(_) => {
-                todo!();
-            }
-            Effect::DatabaseServerStoredEffect(_) => {
-                todo!();
-            }
-            Effect::DomainStoredEffect(_) => {
-                todo!();
-            }
-            Effect::TableStoredEffect(_) => {
-                todo!();
-            }
-            Effect::TableValueEffect(_) => {
-                todo!();
-            }
-            Effect::IndexValueEffect(_) => {
-                todo!();
-            }
-            Effect::ColumnValueEffect(_) => {
-                todo!();
-            }
-        });
+        let cf_opts = rocksdb::Options::default();        
+        let mut db = ra.db_guard().map_db_err_to_status()?;
+        for effect in self.into_iter() {
+            match effect {
+                    Effect::CreateCf(cf_name) => {
+                        db.create_cf(cf_name, &cf_opts).map_err(|_| DbError::RocksDbError).map_db_err_to_status()?;
+                    }
+                    Effect::DeleteCf(cf_name) => {
+                        db.drop_cf(cf_name).map_err(|_| DbError::RocksDbError).map_db_err_to_status()?;
+                    }
+                    Effect::DatabaseServerStoredEffect(_) => {
+                        todo!();
+                    }
+                    Effect::DomainStoredEffect(_) => {
+                        todo!();
+                    }
+                    Effect::TableStoredEffect(_) => {
+                        todo!();
+                    }
+                    Effect::TableValueEffect(_) => {
+                        todo!();
+                    }
+                    Effect::IndexValueEffect(_) => {
+                        todo!();
+                    }
+                    Effect::ColumnValueEffect(_) => {
+                        todo!();
+                    }
+                }
+        }
         Ok(Response::new(EmptyMessage {}))
     }
 }
