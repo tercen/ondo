0ec55552729bc69baeb788613f25e54c94ad6f73
Umur Ozkul <umur@hightechmind.io>
2023-03-16 22:27:17 +0100

    refactor to_reference

--- src/db/server/table_server_trait_impl.rs
+++ src/db/server/table_server_trait_impl.rs
@@ -1,8 +1,54 @@
+use super::db_error_to_status::DbErrorOptionToStatus;
+use super::db_error_to_status::DbErrorToStatus;
 use super::rocks_db_accessor::RocksDbAccessor;
+use super::source_sink::effects_sink::EffectsSink;
 use super::table_server_trait::TableServerTrait;
+use super::to_entity_trait::FromEntity;
+use super::to_entity_trait::ToEntity;
+use super::to_reference_trait::ToReference;
+use crate::db::entity::reference::table_reference::TableReference;
+use crate::db::entity::reference::table_reference::TableReferenceTrait;
+use crate::db::entity::table::Table;
 use crate::ondo_remote;
 use ondo_remote::*;
 use tonic::{Request, Response, Status};
+
+// impl ToReference<TableReference> for Request<TableReferenceMessage> {
+//     fn to_reference(&self) -> TableReference {
+//         TableReference {
+//             domain_name: self.get_ref().domain_name.clone(),
+//         }
+//     }
+// }
+
+// impl ToReference<TableReference> for Request<TableMessage> {
+//     fn to_reference(&self) -> TableReference {
+//         let msg = self.get_ref();
+//         let r_msg = msg.domain_reference.as_ref().unwrap();
+//         TableReference {
+//             domain_name: r_msg.domain_name.clone(),
+//         }
+//     }
+// }
+
+// impl ToEntity<Table> for Request<TableMessage> {
+//     fn to_entity(&self) -> Table {
+//         let r = self.to_reference();
+//         Table { id: r }
+//     }
+// }
+
+// impl FromEntity<Table> for Response<TableMessage> {
+//     fn from_entity(entity: Table) -> Self {
+//         let r = entity.id;
+//         let r_msg = TableReferenceMessage {
+//             domain_name: r.domain_name,
+//         };
+//         Response::new(TableMessage {
+//             domain_reference: Some(r_msg),
+//         })
+//     }
+// }
 
 impl TableServerTrait for RocksDbAccessor {
     fn create_table(&self, _: Request<TableMessage>) -> Result<Response<EmptyMessage>, Status> {
--- src/db/server/to_reference_trait.rs
+++ src/db/server/to_reference_trait.rs
@@ -1,3 +1,7 @@
 pub trait ToReference<T> {
     fn to_reference(&self) -> T;
 }
+
+pub trait FromReference<T> {
+    fn from_reference(reference: T) -> Self;
+}
