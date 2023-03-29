1b60b4891282b22fa196a9133b8a9bb20e2b09a8
Umur Ozkul <umur@hightechmind.io>
2023-03-29 18:29:11 +0200

    tmp rocksdb for unit tests

--- src/db/server/rocks_db_accessor.rs
+++ src/db/server/rocks_db_accessor.rs
@@ -3,6 +3,7 @@ use crate::db::db_error::DbError;
 use crate::db::db_error::DbResult;
 use rocksdb::{Options, DB};
 use std::sync::{Arc, RwLock, RwLockReadGuard};
+use tempfile::TempDir;
 
 // Define the struct that contains the RocksDB instance
 #[derive(Clone)]
@@ -23,32 +24,38 @@ pub struct Version {
 
 impl Default for RocksDbAccessor {
     fn default() -> Self {
-        // Define the default path for the database
         let db_path = std::env::var("ONDO_DB_PATH").unwrap_or("./db/ondo_rocksdb".to_owned());
 
-        // Create options for the RocksDB instance
         let mut options = Options::default();
         options.create_if_missing(true);
-        // options.set_use_thread_local(true);
 
+        Self::init(db_path, options)
+    }
+}
+
+impl RocksDbAccessor {
+    fn init(db_path: String, options: Options) -> Self {
         let cf_names = DB::list_cf(&options, &db_path).unwrap_or(Vec::new());
-
-        // Open the RocksDB instance at the default path
         let raw_db = DB::open_cf(&options, &db_path, cf_names).unwrap();
-
-        // Wrap the RocksDB instance in an Arc and Mutex to ensure thread safety
         let db = Arc::new(RwLock::new(raw_db));
 
-        // Return a new instance of MyStruct with the default RocksDB instance
         RocksDbAccessor {
             db,
             db_path,
             options,
         }
     }
-}
 
-impl RocksDbAccessor {
+    pub fn in_memory() -> Self {
+        let temp_dir = TempDir::new().expect("Failed to create temporary directory");
+        let db_path = temp_dir.path().to_string_lossy().into_owned();
+
+        let mut options = Options::default();
+        options.create_if_missing(true);
+
+        Self::init(db_path, options)
+    }
+
     pub fn guarded_db(&self) -> Arc<RwLock<DB>> {
         Arc::clone(&self.db)
     }
