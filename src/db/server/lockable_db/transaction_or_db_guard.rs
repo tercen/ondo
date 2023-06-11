75b1c41fe8ec6645f4b2705470142f86fb279830
umuro <umur.ozkul@gmail.com>
2023-06-11 15:24:44 +0200

    transaction or db guard

--- src/db/server/lockable_db/transaction_or_db_guard.rs
+++ src/db/server/lockable_db/transaction_or_db_guard.rs
@@ -6,14 +6,21 @@ use std::ops::DerefMut;
 
 use super::{
     db_read_lock_guard_wrapper::DbReadLockGuardWrapper,
-    db_write_lock_guard_wrapper::DbWriteLockGuardWrapper, transaction_or_db::TransactionOrDb,
-    reentrant_mutex_guard_wrapper::ReentrantMutexGuardWrapper,
-}; 
+    db_write_lock_guard_wrapper::DbWriteLockGuardWrapper,
+    reentrant_mutex_guard_wrapper::ReentrantMutexGuardWrapper, transaction_or_db::TransactionOrDb,
+};
+
+type PreferredTransactionReadLockGuardWrapper<'a> =
+    ReentrantMutexGuardWrapper<'a, Transaction<'a, TransactionDB>>;
+type PreferredDbReadLockGuardWrapper<'a> = DbReadLockGuardWrapper<'a, TransactionDB>;
 
 // #[derive(Debug)]
 pub(crate) enum TransactionOrDbReadGuard<'a> {
-    TransactionRead(ReentrantMutexGuardWrapper<'a, Transaction<'a, TransactionDB>>),
-    DbRead(DbReadLockGuardWrapper<'a, TransactionDB>),
+    TransactionRead(
+        PreferredTransactionReadLockGuardWrapper<'a>,
+        PreferredDbReadLockGuardWrapper<'a>,
+    ),
+    DbRead(PreferredDbReadLockGuardWrapper<'a>),
 }
 
 impl<'a> Deref for TransactionOrDbReadGuard<'a> {
@@ -21,18 +28,25 @@ impl<'a> Deref for TransactionOrDbReadGuard<'a> {
 
     fn deref(&self) -> &Self::Target {
         match self {
-            TransactionOrDbReadGuard::TransactionRead(guard) => {
-                &TransactionOrDb::Transaction(guard.deref())
+            TransactionOrDbReadGuard::TransactionRead(guard, db_guard) => {
+                &TransactionOrDb::Transaction(guard.deref(), db_guard.deref())
             }
             TransactionOrDbReadGuard::DbRead(guard) => &TransactionOrDb::Db(guard.deref()),
         }
     }
 }
 
+type PreferredTransactionWriteLockGuardWrapper<'a> =
+    ReentrantMutexGuardWrapper<'a, Transaction<'a, TransactionDB>>;
+type PreferredDbWriteLockGuardWrapper<'a> = DbWriteLockGuardWrapper<'a, TransactionDB>;
+
 // #[derive(Debug)]
 pub(crate) enum TransactionOrDbWriteGuard<'a> {
-    TransactionWrite(ReentrantMutexGuardWrapper<'a, Transaction<'a, TransactionDB>>),
-    DbWrite(DbWriteLockGuardWrapper<'a, TransactionDB>),
+    TransactionWrite(
+        PreferredTransactionWriteLockGuardWrapper<'a>,
+        PreferredDbWriteLockGuardWrapper<'a>,
+    ),
+    DbWrite(PreferredDbWriteLockGuardWrapper<'a>),
 }
 
 impl<'a> Deref for TransactionOrDbWriteGuard<'a> {
@@ -40,8 +54,8 @@ impl<'a> Deref for TransactionOrDbWriteGuard<'a> {
 
     fn deref(&self) -> &Self::Target {
         match self {
-            TransactionOrDbWriteGuard::TransactionWrite(guard) => {
-                &TransactionOrDb::Transaction(guard.deref())
+            TransactionOrDbWriteGuard::TransactionWrite(guard, db_guard) => {
+                &TransactionOrDb::Transaction(guard.deref(), db_guard.deref())
             }
             TransactionOrDbWriteGuard::DbWrite(guard) => &TransactionOrDb::Db(guard.deref()),
         }
@@ -51,8 +65,8 @@ impl<'a> Deref for TransactionOrDbWriteGuard<'a> {
 impl<'a> DerefMut for TransactionOrDbWriteGuard<'a> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         match self {
-            TransactionOrDbWriteGuard::TransactionWrite(guard) => {
-                &mut TransactionOrDb::Transaction(guard.deref_mut())
+            TransactionOrDbWriteGuard::TransactionWrite(guard, db_guard) => {
+                &mut TransactionOrDb::Transaction(guard.deref_mut(), db_guard.deref_mut())
             }
             TransactionOrDbWriteGuard::DbWrite(guard) => {
                 &mut TransactionOrDb::Db(guard.deref_mut())
