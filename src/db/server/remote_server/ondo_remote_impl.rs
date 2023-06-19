ea46a74f24cc447ce36aa6436c1bea46eeb88322
umuro <umur.ozkul@gmail.com>
2023-06-19 13:35:13 +0200

    blocking transaction loop

--- src/db/server/remote_server/ondo_remote_impl.rs
+++ src/db/server/remote_server/ondo_remote_impl.rs
@@ -5,7 +5,7 @@ use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::StreamExt;
 
-use super::send_response::send_response;
+use super::send_response::{blocking_send_response, send_response};
 use crate::db::server::lockable_db::transaction_maker::{
     LockableTransactionOrDb, TransactionMaker,
 };
@@ -33,74 +33,23 @@ impl ondo_remote_server::OndoRemote for MyServer {
         &self,
         request: tonic::Request<tonic::Streaming<TransactionRequest>>,
     ) -> Result<tonic::Response<Self::TransactionStreamStream>, tonic::Status> {
-        let (tx, rx) = mpsc::channel(16);
+        let (answer_tx, answer_rx) = mpsc::channel(16);
+        let (client_tx, client_rx) = mpsc::channel(16);
 
         let mut stream = request.into_inner();
-
-        //FIXME: Get local transaction here instead of db clone
         let my_server_clone = self.clone();
         tokio::spawn(async move {
-            let lockable_db = LOCKABLE_DB.clone();
-            let db_guard = lockable_db.read();
-            let transaction = db_guard.transaction();
-
-            let lockable_transaction = LockableTransactionOrDb {
-                transaction: Some(Arc::new(ReentrantMutex::new(transaction))),
-                lockable_db: lockable_db.clone(),
-            };
-
+            let _ = tokio::task::spawn_blocking(move || {
+                transaction_loop(my_server_clone, client_rx, answer_tx.clone());
+            });
+            tokio::task::yield_now().await;
             while let Some(request) = stream.next().await {
-                match request {
-                    Ok(transaction_request) => {
-                        match transaction_request.request_type {
-                            Some(transaction_request::RequestType::TableValueOps(
-                                table_value_ops,
-                            )) => {
-                                let response_type = my_server_clone
-                                    .table_value_ops_sub_server(lockable_transaction.clone())
-                                    .process_request(
-                                        tx.clone(),
-                                        table_value_ops.request_type.unwrap(),
-                                    );
-                                send_response(tx.clone(), response_type);
-                            }
-                            Some(transaction_request::RequestType::IndexedValueOps(
-                                indexed_value_ops,
-                            )) => {
-                                let response_type = my_server_clone
-                                    .indexed_value_ops_sub_server(lockable_transaction.clone())
-                                    .process_request(
-                                        tx.clone(),
-                                        indexed_value_ops.request_type.unwrap(),
-                                    );
-                                send_response(tx.clone(), response_type);
-                            }
-                            Some(transaction_request::RequestType::KeyPrefixOps(
-                                key_prefix_ops,
-                            )) => {
-                                let response_type = my_server_clone
-                                    .key_prefix_ops_sub_server(lockable_transaction.clone())
-                                    .process_request(
-                                        tx.clone(),
-                                        key_prefix_ops.request_type.unwrap(),
-                                    );
-                                send_response(tx.clone(), response_type);
-                            }
-                            None => {
-                                // You could return an error here if you want
-                            }
-                        }
-                    }
-                    Err(err) => {
-                        // Handle stream error
-                        eprintln!("Error receiving request: {:?}", err);
-                        break;
-                    }
-                }
+                tokio::task::yield_now().await;
+                client_tx.send(request).await;
             }
         });
 
-        let response_stream = ReceiverStream::new(rx);
+        let response_stream = ReceiverStream::new(answer_rx);
 
         Ok(tonic::Response::new(
             Box::pin(response_stream) as Self::TransactionStreamStream
@@ -118,7 +67,7 @@ impl ondo_remote_server::OndoRemote for MyServer {
         let my_server_clone = self.clone();
         tokio::spawn(async move {
             let transaction_maker = TransactionMaker::new(LOCKABLE_DB.clone());
-            let lockable_db = transaction_maker.lockable_db();
+            let lockable_transaction_or_db = transaction_maker.lockable_db();
             while let Some(request) = stream.next().await {
                 match request {
                     Ok(transaction_request) => {
@@ -127,7 +76,7 @@ impl ondo_remote_server::OndoRemote for MyServer {
                                 table_value_ops,
                             )) => {
                                 let response_type = my_server_clone
-                                    .table_value_ops_sub_server(lockable_db.clone())
+                                    .table_value_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(
                                         tx.clone(),
                                         table_value_ops.request_type.unwrap(),
@@ -138,7 +87,7 @@ impl ondo_remote_server::OndoRemote for MyServer {
                                 indexed_value_ops,
                             )) => {
                                 let response_type = my_server_clone
-                                    .indexed_value_ops_sub_server(lockable_db.clone())
+                                    .indexed_value_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(
                                         tx.clone(),
                                         indexed_value_ops.request_type.unwrap(),
@@ -149,7 +98,7 @@ impl ondo_remote_server::OndoRemote for MyServer {
                                 key_prefix_ops,
                             )) => {
                                 let response_type = my_server_clone
-                                    .key_prefix_ops_sub_server(lockable_db.clone())
+                                    .key_prefix_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(
                                         tx.clone(),
                                         key_prefix_ops.request_type.unwrap(),
@@ -189,14 +138,14 @@ impl ondo_remote_server::OndoRemote for MyServer {
         let my_server_clone = self.clone();
         tokio::spawn(async move {
             let transaction_maker = TransactionMaker::new(LOCKABLE_DB.clone());
-            let lockable_db = transaction_maker.lockable_db();
+            let lockable_transaction_or_db = transaction_maker.lockable_db();
             while let Some(request) = stream.next().await {
                 match request {
                     Ok(meta_request) => {
                         match meta_request.request_type {
                             Some(meta_request::RequestType::VersionRequest(version_request)) => {
                                 let response_type = my_server_clone
-                                    .version_sub_server(lockable_db.clone())
+                                    .version_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(tx.clone(), version_request);
                                 send_response(tx.clone(), response_type);
                             }
@@ -204,7 +153,7 @@ impl ondo_remote_server::OndoRemote for MyServer {
                                 database_server_ops,
                             )) => {
                                 let response_type = my_server_clone
-                                    .database_server_ops_sub_server(lockable_db.clone())
+                                    .database_server_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(
                                         tx.clone(),
                                         database_server_ops.request_type.unwrap(),
@@ -213,25 +162,25 @@ impl ondo_remote_server::OndoRemote for MyServer {
                             }
                             Some(meta_request::RequestType::DomainOps(domain_ops)) => {
                                 let response_type = my_server_clone
-                                    .domain_ops_sub_server(lockable_db.clone())
+                                    .domain_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(tx.clone(), domain_ops.request_type.unwrap());
                                 send_response(tx.clone(), response_type);
                             }
                             Some(meta_request::RequestType::TableOps(table_ops)) => {
                                 let response_type = my_server_clone
-                                    .table_ops_sub_server(lockable_db.clone())
+                                    .table_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(tx.clone(), table_ops.request_type.unwrap());
                                 send_response(tx.clone(), response_type);
                             }
                             Some(meta_request::RequestType::IndexOps(index_ops)) => {
                                 let response_type = my_server_clone
-                                    .index_ops_sub_server(lockable_db.clone())
+                                    .index_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(tx.clone(), index_ops.request_type.unwrap());
                                 send_response(tx.clone(), response_type);
                             }
                             Some(meta_request::RequestType::TextIndexOps(text_index_ops)) => {
                                 let response_type = my_server_clone
-                                    .text_index_ops_sub_server(lockable_db.clone())
+                                    .text_index_ops_sub_server(lockable_transaction_or_db.clone())
                                     .process_request(
                                         tx.clone(),
                                         text_index_ops.request_type.unwrap(),
@@ -257,5 +206,65 @@ impl ondo_remote_server::OndoRemote for MyServer {
         Ok(tonic::Response::new(
             Box::pin(response_stream) as Self::TransactionStreamStream
         ))
+    }
+}
+
+//FIXME: add transaction commit and abort
+fn transaction_loop(
+    my_server_clone: MyServer,
+    mut client_rx: tokio::sync::mpsc::Receiver<Result<TransactionRequest, tonic::Status>>,
+    answer_tx: tokio::sync::mpsc::Sender<Result<TransactionResponse, tonic::Status>>,
+) {
+    let lockable_db = LOCKABLE_DB.clone();
+    let db_guard = lockable_db.read();
+    let transaction = db_guard.transaction();
+
+    let lockable_transaction = LockableTransactionOrDb {
+        transaction: Some(Arc::new(ReentrantMutex::new(transaction))),
+        lockable_db: lockable_db.clone(),
+    };
+
+    while let Some(request) = client_rx.blocking_recv() {
+        match request {
+            Ok(transaction_request) => {
+                match transaction_request.request_type {
+                    Some(transaction_request::RequestType::TableValueOps(table_value_ops)) => {
+                        let response_type = my_server_clone
+                            .table_value_ops_sub_server(lockable_transaction.clone())
+                            .process_request(
+                                answer_tx.clone(),
+                                table_value_ops.request_type.unwrap(),
+                            );
+                        blocking_send_response(answer_tx.clone(), response_type);
+                    }
+                    Some(transaction_request::RequestType::IndexedValueOps(indexed_value_ops)) => {
+                        let response_type = my_server_clone
+                            .indexed_value_ops_sub_server(lockable_transaction.clone())
+                            .process_request(
+                                answer_tx.clone(),
+                                indexed_value_ops.request_type.unwrap(),
+                            );
+                        blocking_send_response(answer_tx.clone(), response_type);
+                    }
+                    Some(transaction_request::RequestType::KeyPrefixOps(key_prefix_ops)) => {
+                        let response_type = my_server_clone
+                            .key_prefix_ops_sub_server(lockable_transaction.clone())
+                            .process_request(
+                                answer_tx.clone(),
+                                key_prefix_ops.request_type.unwrap(),
+                            );
+                        blocking_send_response(answer_tx.clone(), response_type);
+                    }
+                    None => {
+                        // You could return an error here if you want
+                    }
+                }
+            }
+            Err(err) => {
+                // Handle stream error
+                eprintln!("Error receiving request: {:?}", err);
+                break;
+            }
+        }
     }
 }
--- src/db/server/remote_server/send_response.rs
+++ src/db/server/remote_server/send_response.rs
@@ -22,3 +22,25 @@ pub(super) async fn send_status_response(
         eprintln!("Error sending response: {:?}", err);
     }
 }
+
+pub(super) fn blocking_send_response(
+    tx: tokio::sync::mpsc::Sender<Result<TransactionResponse, Status>>,
+    response_type: ResponseType,
+) {
+    let response = TransactionResponse {
+        response_type: Some(response_type),
+    };
+
+    if let Err(err) = tx.blocking_send(Ok(response)) {
+        eprintln!("Error sending response: {:?}", err);
+    }
+}
+
+pub(super) fn blocking_send_status_response(
+    tx: tokio::sync::mpsc::Sender<Result<TransactionResponse, Status>>,
+    status: Status,
+) {
+    if let Err(err) = tx.blocking_send(Err(status)) {
+        eprintln!("Error sending response: {:?}", err);
+    }
+}
