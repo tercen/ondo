dc448f93d18c35f4429c5272856376426d811a2e
umuro <umur.ozkul@gmail.com>
2023-06-22 08:23:59 +0200

    spell

--- src/db/server/lockable_db/transaction_or_db_guard.rs
+++ src/db/server/lockable_db/transaction_or_db_guard.rs
@@ -43,8 +43,8 @@ impl<'a, 'b> TransactionOrDbReadGuard<'a, 'b> {
     }
     pub(crate) fn inner_older(&'a self) -> TransactionOrDb<'a> {
         match &self.guard_pair {
-            (db_guard, Some(transantion_guard)) => {
-                TransactionOrDb::Transaction(transantion_guard.deref(), db_guard.deref())
+            (db_guard, Some(transaction_guard)) => {
+                TransactionOrDb::Transaction(transaction_guard.deref(), db_guard.deref())
             }
             (db_guard, None) => TransactionOrDb::Db(db_guard.deref()),
         }
@@ -75,15 +75,15 @@ impl<'a, 'b> TransactionOrDbWriteGuard<'a, 'b> {
 
     pub(crate) fn inner(&'a self) -> TransactionOrDb<'a> {
         match &self.guard_pair {
-            (db_guard, Some(transantion_guard)) => {
-                TransactionOrDb::Transaction(transantion_guard.deref(), db_guard.deref())
+            (db_guard, Some(transaction_guard)) => {
+                TransactionOrDb::Transaction(transaction_guard.deref(), db_guard.deref())
             }
             (db_guard, None) => TransactionOrDb::Db(db_guard.deref()),
         }
     }
     pub(crate) fn inner_mut<'c>(&'c mut self) -> MutTransactionOrDb<'c> {
         match &self.guard_pair {
-            (db_guard, Some(transantion_guard)) => MutTransactionOrDb::Transaction,
+            (db_guard, Some(transaction_guard)) => MutTransactionOrDb::Transaction,
             (ref mut  db_guard, None) => MutTransactionOrDb::Db(db_guard.deref_mut()),
         }
     }
